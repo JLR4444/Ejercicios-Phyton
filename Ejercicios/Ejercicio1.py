04cf731ba45b11e495fa0d53f247f1b2d9ce15d5
Jenniffer <jlrlorente4@gmail.com>
2026-04-18 01:24:47 +0200

    Ejercicio 1 python variables

--- Ejercicios/Ejercicio1.py
+++ Ejercicios/Ejercicio1.py
@@ -1,1 +1,20 @@
-print("Hola mundo")
+#Información película favorita
+#Cadena
+Título = "Harry Potter y el prisionero de Azkaban"
+Director = "Alfonso Cuarón"
+Genero = "Fantasía"
+#Numeros (int)
+Año = 2004
+Duración_minutos = 142
+#Numeros con decimales (float)
+
+#Booleanos (True o False)
+Tiene_premios = True
+
+#Imprimir las variables
+print("Título", Título)
+print("Director", Director)
+print("Genero ", Genero)
+print("Año ", Año)
+print("Duración_minutos", Duración_minutos)
+print("Tiene_premios? ", Tiene_premios)
