8c2b1d9335cc3f89257afdcca0188962e79fade5
Jenniffer <jlrlorente4@gmail.com>
2026-04-18 01:35:45 +0200

    Ejercicio 1 en inglés

---
+++ Ejercicios/Exercise1.py
@@ -0,0 +1,20 @@
+#Información película favorita
+#Cadena
+Título = "Harry Potter and the Prisoner of Azkaban"
+Director = "Alfonso Cuarón"
+Genero = "Fantasy"
+#Numeros (int)
+Año = 2004
+Duración_minutos = 142
+#Numeros con decimales (float)
+
+#Booleanos (True o False)
+Tiene_premios = True
+
+#Imprimir las variables
+print("Tittle", Título)
+print("Director", Director)
+print("Gender ", Genero)
+print("Year ", Año)
+print("Duration", Duración_minutos)
+print("Has awards? ", Tiene_premios)
