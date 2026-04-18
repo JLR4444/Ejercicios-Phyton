37711e39dbcf8edf5fa4a77c050be4cb7bed6f36
Jenniffer <jlrlorente4@gmail.com>
2026-04-18 16:51:53 +0200

    Ejercicio 2 del tema 1: Canciones

---
+++ Ejercicios/T1E2Canciones.py
@@ -0,0 +1,21 @@
+#Información canción favorita
+#Cadena
+Título = "Despacito"
+Artista = "Luis Fonsi & Daddy Yankee"
+Álbum = "Despacito"
+#Numeros (int)
+Año = 2017
+#Numeros con decimales (float)
+Duración_segundos = 247.2
+
+#Booleanos (True o False)
+Tiene_videoclip = True
+
+#Imprimir las variables
+print("Título", Título)
+print("Artista", Artista)
+print("Álbum ", Álbum)
+print("Año ", Año)
+print("Duración_segundos", Duración_segundos
+)
+print("Tiene_videoclip? ", Tiene_videoclip)
