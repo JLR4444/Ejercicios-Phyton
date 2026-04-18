f3440b9ce3efa52aa583745cddde0ad9e061c960
Jenniffer <jlrlorente4@gmail.com>
2026-04-18 16:34:21 +0200

    Cambios de nombre en los ejercicios y Ejercicio1 del segundo tema

---
+++ Ejercicios/T2E1Calculadora.py
@@ -0,0 +1,20 @@
+#Pedir al usuario que introduzca 2 numeros
+#Convertimos a numero. int (sin decimales), float(puede tener decimales)
+numero1 = 4
+numero2 = 24
+
+#Hacemos las operaciones
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+media = (numero1 + numero2) / 2
+
+#Imprimimos los resultados
+print("Suma ", suma)
+print("Resta ", resta)
+print("Multiplicacion ", multiplicacion)
+print("Division ", division)
+print("Media ", media)
