b12d0eb3f62d0475ab084cd6c3b443aafdfc3e0d
Jenniffer <jlrlorente4@gmail.com>
2026-04-26 18:14:28 +0200

    Ejercicio1 T4 Relueta de colores

---
+++ Ejercicios/T4E1Ruletacolores.py
@@ -0,0 +1,13 @@
+color = input("Elige un color (Rojo, Verde, Azul, Amarillo, Morado): ")
+if color == "Rojo":
+    print("Pasión y energía")
+elif color == "Verde":
+    print("Esperanza y crecimiento")
+elif color == "Azul":
+    print("Calma y serenidad")
+elif color == "Amarillo":
+    print("Felicidad y optimismo")
+elif color == "Morado":
+    print("Sabiduría y creatividad")
+else:
+    print("Color no válido")
