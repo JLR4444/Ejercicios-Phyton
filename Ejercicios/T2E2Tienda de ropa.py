eeaefd9237bc9034d02a50b73d9aef0d197d9e65
Jenniffer <jlrlorente4@gmail.com>
2026-04-26 18:48:09 +0200

    Solucion al ejercicio 2 T2 Tienda de ropa

--- Ejercicios/T2E2Tienda de ropa.py
+++ Ejercicios/T2E2Tienda de ropa.py
@@ -1,27 +1,23 @@
 #Definir los precios
-precio_camiseta = 10
-precio_sudadera = 20.5
-precio_gorra = 5.5
+camiseta = 10
+sudadera = 20.5
+gorra = 5.5
 
 #Pedir cantidades de cada artículo
-cantidad_camisetas = input("Cuantas camisetas quieres? ")
-cantidad_sudaderas = input("Cuantas sudaderas quieres? ")
-cantidad_gorras = input("Cuantas gorras quieres? ")
+cantidad_camisetas = int(input("¿Cuántas camisetas quieres?"))
+cantidad_sudaderas = int(input("¿Cuántas sudaderas quieres?"))
+cantidad_gorras = int(input("¿Cuántas gorras quieres?"))
 
 #Total de la compra
-precio_camiseta * cantidad_camisetas
-precio_sudadera * cantidad_sudaderas
-precio_gorra * cantidad_gorras
+precio_total_camisetas = cantidad_camisetas * camiseta
+precio_total_sudaderas = cantidad_sudaderas * sudadera
+precio_total_gorras = cantidad_gorras * gorra
+
+
+precio_total_compra = precio_total_camisetas + precio_total_sudaderas + precio_total_gorras
 
 #Añade el 21% de IVA al total.
-iva = total * 0.21
+precio_total_con_iva = precio_total_compra + (precio_total_compra * 0.21)
 
-#Precio final   --> DUDO SI SERÍA CORRECTO
-precio_final = precio_total + iva * 0.21
-
-#Imprimimos los resultados   --> NO SE COMO SERÍA... HABRIA QUE PREGUNTAR O AÑADIR LAS CANTIDADES NO?
-print(" ", )
-print(" ", )
-print(" ", )
-print(" ", )
-print(" ", )
+#Imprimimos los resultados
+print("precio total compra con iva: ", precio_total_con_iva)
---
+++ Ejercicios/T5E1Calculdora.py
@@ -0,0 +1,11 @@
+#Pide a la usuaria cuántas notas desea introducir. 
+cantidad = int(input("¿Cuántas notas quieres introducir?: "))
+
+
+for i in range(cantidad):
+    nota = float(input("Introduce una nota: "))
+    suma = suma + nota
+
+media = suma / cantidad
+
+print("La media es:", media)
