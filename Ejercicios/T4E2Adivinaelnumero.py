79e19fec7e062b4fa89b7212b4373fe82027a557
Jenniffer <jlrlorente4@gmail.com>
2026-04-26 18:27:09 +0200

    Ejercicio2 T4 Adivina el numero condicionales

---
+++ Ejercicios/T4E2Adivinaelnumero.py
@@ -0,0 +1,9 @@
+numero = int(input("Elige un numero entre el 1 y el 10: "))
+if numero == 4:
+    print("¡Has ganado!")
+elif numero < 4:
+    print("Has perdido...")
+elif numero > 4:
+    print("Has perdido...")
+else:
+    print("¡Has ganado!")
